95ac12c66066f8038052885d35b03bd2b498a558
huz10 <@hr928919122>
2024-09-02 10:14:48 +0800

    spring

--- spring/spring-framework/src/main/java/com/stack/spring/metadata/loader/AppRunner.java
+++ spring/spring-framework/src/main/java/com/stack/spring/metadata/loader/AppRunner.java
@@ -1,6 +1,7 @@
 package com.stack.spring.metadata.loader;
 
 import com.stack.spring.metadata.loader.config.AppConfiguration;
+import com.stack.spring.metadata.loader.config.CglibEnhancerConfiguration;
 import com.stack.spring.metadata.loader.service.HelloWorldService;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
@@ -12,9 +13,13 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 public class AppRunner {
 
     public static void main(String[] args) {
-        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AppConfiguration.class);
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AppConfiguration.class, CglibEnhancerConfiguration.class);
         HelloWorldService helloWorldService = context.getBean(HelloWorldService.class);
         helloWorldService.sayHello();
+
+        CglibEnhancerConfiguration cglibEnhancerConfiguration = context.getBean(CglibEnhancerConfiguration.class);
+        System.out.println(cglibEnhancerConfiguration.something());
+        System.out.println(cglibEnhancerConfiguration.something());
     }
 
 }
--- spring/spring-framework/src/main/java/com/stack/spring/metadata/loader/config/CglibEnhancerConfiguration.java
+++ spring/spring-framework/src/main/java/com/stack/spring/metadata/loader/config/CglibEnhancerConfiguration.java
@@ -1,5 +1,6 @@
 package com.stack.spring.metadata.loader.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 /**
@@ -10,5 +11,11 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class CglibEnhancerConfiguration {
 
+    private int counter;
+
+    @Bean
+    public String something() {
+        return String.valueOf(++counter);
+    }
 
 }
